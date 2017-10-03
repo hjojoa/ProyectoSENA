16f4602f64d31dbeb451bdc4171dce08f1e6062f
Hamilton Jojoa <hjojoa@unicauca.edu.co>
2017-10-03 15:36:09 -0500

    Proyecto SENA

---
+++ ProyectoSENA/app/src/main/java/com/example/hamiltonjojoa/proyectosena/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.hamiltonjojoa.proyectosena;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
